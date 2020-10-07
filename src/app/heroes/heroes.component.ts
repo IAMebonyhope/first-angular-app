c19fe796dba86633f76338d6edcc765d71273d8c
ebonyhope <blessyn2hope@gmail.com>
2020-10-07 23:45:47 +0100

    learnt about routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,12 +12,6 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
